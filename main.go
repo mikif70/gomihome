b791890dd78ee28e32cd17ebde5ebba66f5aad56
Michele <mikif70@gmail.com>
2017-11-20 10:47:50 +0100

    test 2

--- main.go
+++ main.go
@@ -1,31 +1,37 @@
-// main
-package main
+package xiaomi
 
 import (
 	//	"encoding/hex"
-	"fmt"
+	"encoding/json"
 	"log"
 	"net"
-	"time"
+	//	"time"
 )
 
 type (
-	Gateway struct {
-		Addr  *net.UDPAddr
-		Sid   string
-		Token string
+	Response struct {
+		Cmd   string `json:"cmd"`
+		Model string `json:"model"`
+		Sid   string `json:"sid"`
+		//ShortId int `json:"short_id,integer"`
+		Token string      `json:"token,omitempty"`
+		IP    string      `json:"ip,omitempty"`
+		Port  string      `json:"port,omitempty"`
+		Data  interface{} `json:"data"`
 	}
 
 	Request struct {
 		Cmd string `json:"cmd"`
 		Sid string `json:"sid,omitempty"`
 	}
-)
 
-const (
-	multicastIp     = "224.0.0.50"
-	multicastPort   = "9898"
-	maxDatagramSize = 8192
+	Gateway struct {
+		Addr  *net.UDPAddr
+		Sid   string
+		Token string
+	}
+
+	object map[string]interface{}
 )
 
 var (
@@ -33,19 +39,82 @@ var (
 	gateways map[string]Gateway
 )
 
-func main() {
-	ping(multicastIp + ":" + multicastPort)
+const (
+	multicastIp   = "224.0.0.50"
+	multicastPort = "9898"
+	//multicastAddr         = "239.255.255.250:9898"
+	maxDatagramSize = 8192
+)
+
+func GetStatus() string { //test
+	return ""
 }
 
-func ping(a string) {
-	addr, err := net.ResolveUDPAddr("udp", a)
+func (this *Gateway) sendMessage(msg string) {
+	sendMessage(this.Addr, msg)
+}
+
+func sendMessage(addr *net.UDPAddr, msg string) {
+	req, err := json.Marshal(Request{Cmd: msg})
 	if err != nil {
-		fmt.Printf("Err: %+v\n", err)
 		log.Fatal(err)
 	}
-	c, err := net.DialUDP("udp", nil, addr)
-	for {
-		c.Write([]byte("hello, world\n"))
-		time.Sleep(1 * time.Second)
+	log.Println(string(req))
+	log.Println(addr)
+	conn.WriteMsgUDP([]byte(req), nil, addr)
+}
+
+func connHandler() {
+	pingAddr, err := net.ResolveUDPAddr("udp", multicastIp+":4321")
+	if err != nil {
+		log.Fatal(err)
 	}
+	sendMessage(pingAddr, "whois")
+}
+
+func msgHandler(resp *Response) {
+	switch resp.Cmd {
+	case "iam":
+		log.Printf("%+v", resp)
+		_, err := net.ResolveUDPAddr("udp", resp.IP+":"+multicastPort)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	case "heartbeat":
+		log.Printf("%+v", resp)
+	}
+}
+
+func serveMulticastUDP(a string, connectedHandler func(), msgHandler func(resp *Response)) {
+	addr, err := net.ResolveUDPAddr("udp", a)
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn, err = net.ListenMulticastUDP("udp", nil, addr)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	conn.SetReadBuffer(maxDatagramSize)
+	for {
+		b := make([]byte, maxDatagramSize)
+		n, _, err := conn.ReadFromUDP(b)
+		if err != nil {
+			log.Fatal("ReadFromUDP failed:", err)
+		}
+		//log.Println(n, "bytes read from", src)
+		//log.Println(string(b[:n]))
+
+		resp := Response{}
+		err = json.Unmarshal(b[:n], &resp)
+		if err != nil {
+			log.Fatal(err)
+		}
+		msgHandler(&resp)
+	}
+}
+
+func main() {
+	serveMulticastUDP(multicastIp+":"+multicastPort, connHandler, msgHandler)
 }
