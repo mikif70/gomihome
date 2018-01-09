967da865bcea78a78c510e47e2345aef1bcf702b
miki <miki@macpro.miki.sys>
2018-01-09 12:21:08 +0100

    run.sh

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-docker run \
+docker run -d \
         --rm \
         -v /opt/gomihome:/data \
         --name gomihome \
+	--net host \
+	 --restart=unless-stopped \
+        -h gomihome \
         --log-opt max-size=2m \
         --log-opt max-file=5 \
         mikif70/gomihome:1.1.4 \
+		/bin/gomihome \
                 -i 172.17.0.1:8086 \
                 -D \
-				-l /data/gomihome.log \
-				-t 15m \
+		-l /data/gomihome.log \
+		-t 15m \
                 $@
