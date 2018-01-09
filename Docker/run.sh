bfc11bf6a260beabc9c0f553ba168ae80c127b8f
Michele <mikif70@gmail.com>
2018-01-09 11:47:02 +0100

    Docker

---
+++ Docker/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker run \
+        --rm \
+        -v /opt/gomihome:/data \
+        --name gomihome \
+        --log-opt max-size=2m \
+        --log-opt max-file=5 \
+        mikif70/gomihome:1.1.2 \
+                -i 172.17.0.1:8086 \
+                -D \
+				-l /data/gomihome.log \
+				-t 15m \
+                $@
